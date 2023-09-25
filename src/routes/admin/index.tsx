06973908dcc332fa8a9135d6972c6608c5755db4
Daniel Zotti <daniel.zotti87@gmail.com>
2023-09-25 12:16:03 +0200

    Basic functionalities

--- src/routes/admin/index.tsx
+++ src/routes/admin/index.tsx
@@ -6,98 +6,101 @@ import { Logout } from "~/components/auth/logout/logout";
 import { supabaseClient } from "~/supabase/supabase-client";
 import type { User } from "@supabase/supabase-js";
 import { Participant } from "~/types/participant.types";
-import {Link} from "@builder.io/qwik-city";
+import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
-    const user = useSignal<User | null>(null);
+  const user = useSignal<User | null>(null);
 
-    const people = useSignal<Array<Participant> | null>();
+  const people = useSignal<Array<Participant> | null>();
 
-    useVisibleTask$(async () => {
-        const {
-            data: { user: userInfo },
-        } = await supabaseClient.auth.getUser();
-        user.value = userInfo;
+  useVisibleTask$(async () => {
+    const {
+      data: { user: userInfo },
+    } = await supabaseClient.auth.getUser();
+    user.value = userInfo;
 
-        const { data: participantList } = await supabaseClient
-            .from("users")
-            .select("*");
+    const { data: participantList } = await supabaseClient
+      .from("users")
+      .select("*")
+      .order("email", { ascending: true });
 
-        people.value = participantList;
+    people.value = participantList;
 
-        /*if (userInfo?.email) {
+    /*if (userInfo?.email) {
                   const { data: otherInfo } = await supabaseClient
                     .from("users")
                     .select("*")
                     .eq("email", userInfo.email);
                   console.log({ otherInfo });
                 }*/
-    });
+  });
 
-    if (!user.value) {
-        return <Login />;
-    }
+  if (!user.value) {
+    return <Login />;
+  }
 
-    return (
-        <>
-            <h1>Welcome to Fortigames 2023</h1>
+  return (
+    <>
+      <h1>Welcome to Fortigames 2023</h1>
 
-            <Logout />
+      <Logout />
 
-            <pre>Current user: {user.value.email}</pre>
+      <pre>Current user: {user.value.email}</pre>
 
-            {people.value && (
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>N.</th>
-                        <th>Team</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Company</th>
-                        <th>Form</th>
-                        <th>Soccer</th>
-                        <th>Volley</th>
-                        <th>Ping Pong</th>
-                        <th>Board Games</th>
-                        <th>Referee</th>
-                        <th>Facilitator</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {people.value.map((p) => (
-                        <tr key={p.id}>
-                            <td>{p.id}</td>
-                            <td>{p.number}</td>
-                            <td>{p.team}</td>
-                            <td>{p.firstname}</td>
-                            <td>{p.lastname}</td>
-                            <td><Link href={"/admin/" + p.id}>{p.email}</Link></td>
-                            <td>{p.company}</td>
-                            <td>{p.has_filled_form ? "X" : ""}</td>
-                            <td>{p.is_playing_soccer ? "X" : ""}</td>
-                            <td>{p.is_playing_volley ? "X" : ""}</td>
-                            <td>{p.is_playing_pingpong ? "X" : ""}</td>
-                            <td>{p.is_playing_boardgames ? "X" : ""}</td>
-                            <td>{p.is_referee ? "X" : ""}</td>
-                            <td>{p.is_facilitator ? "X" : ""}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            )}
-        </>
-    );
+      {people.value && (
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>N.</th>
+              <th>Team</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Company</th>
+              <th>Form</th>
+              <th>Soccer</th>
+              <th>Volley</th>
+              <th>Ping Pong</th>
+              <th>Board Games</th>
+              <th>Referee</th>
+              <th>Facilitator</th>
+            </tr>
+          </thead>
+          <tbody>
+            {people.value.map((p) => (
+              <tr key={p.id}>
+                <td>{p.id}</td>
+                <td>{p.number}</td>
+                <td>{p.team}</td>
+                <td>{p.firstname}</td>
+                <td>{p.lastname}</td>
+                <td>
+                  <Link href={"/admin/" + p.id}>{p.email}</Link>
+                </td>
+                <td>{p.company}</td>
+                <td>{p.has_filled_form ? "X" : ""}</td>
+                <td>{p.is_playing_soccer ? "X" : ""}</td>
+                <td>{p.is_playing_volley ? "X" : ""}</td>
+                <td>{p.is_playing_pingpong ? "X" : ""}</td>
+                <td>{p.is_playing_boardgames ? "X" : ""}</td>
+                <td>{p.is_referee ? "X" : ""}</td>
+                <td>{p.is_facilitator ? "X" : ""}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 });
 
 export const head: DocumentHead = {
-    title: "Fortigames 2023 Admin",
-    meta: [
-        {
-            name: "description",
-            content: "Fortigames 2023 description",
-        },
-    ],
+  title: "Fortigames 2023 Admin",
+  meta: [
+    {
+      name: "description",
+      content: "Fortigames 2023 description",
+    },
+  ],
 };
--- src/routes/info/index.tsx
+++ src/routes/info/index.tsx
@@ -19,35 +19,35 @@ export default component$(() => {
         voluptate voluptatem.
       </p>
       <h2>Comportamento</h2>
-      <p>
-          testo
-      </p>
+      <p>testo</p>
 
       <h2>Calcolo Punteggi</h2>
       <h3>Calcio</h3>
       <p>
-          La partita dura 2 ore e vince la partita chi segna più gol
-          L’arbitro segna “+1” a ogni gol
-          Il risultato finale sarà in base ai gol:  Dragons 8 gol - Tigers 4 gol: vincono i Dragons
-          
+        La partita dura 2 ore e vince la partita chi segna più gol L’arbitro
+        segna “+1” a ogni gol Il risultato finale sarà in base ai gol: Dragons 8
+        gol - Tigers 4 gol: vincono i Dragons
       </p>
       <h3>Pallavolo</h3>
       <p>
-          La partita dura 2 ore e vince la partita chi vince più set (a 25 punti)
-          L’arbitro segna “+1" a ogni SET vinto
-          Il risultato finale sarà in base ai set: Dragons 2 set - Tigers 4 set: vincono i Tigers
+        La partita dura 2 ore e vince la partita chi vince più set (a 25 punti)
+        L’arbitro segna “+1" a ogni SET vinto Il risultato finale sarà in base
+        ai set: Dragons 2 set - Tigers 4 set: vincono i Tigers
       </p>
       <h3>Ping pong</h3>
       <p>
-          La partita dura 2 ore e vince la partita chi vince più set (a 21 punti)
-          L’arbitro segna “+1” a ogni SET vinto (ES: Bruno 21 - Chiarello 18, vince il set Bruno ed è “+1" per i Dragons)
-          Il risultato finale sarà in base ai set vinto: Dragons 3 set - Tigers  7 set: vincono i Tigers
-          (Il set nel ping pong è la partita tra Bruno e Chiarello)
+        La partita dura 2 ore e vince la partita chi vince più set (a 21 punti)
+        L’arbitro segna “+1” a ogni SET vinto (ES: Bruno 21 - Chiarello 18,
+        vince il set Bruno ed è “+1" per i Dragons) Il risultato finale sarà in
+        base ai set vinto: Dragons 3 set - Tigers 7 set: vincono i Tigers (Il
+        set nel ping pong è la partita tra Bruno e Chiarello)
       </p>
 
       <h2>Condizioni di Vittoria</h2>
       <p>
-          I Dragons hanno vinto calcio (1), i Tigers hanno vinto ping pong e pallavolo (2) --> Il torneo lo vincono i Dragons
+        I Dragons hanno vinto calcio (1), i Tigers hanno vinto ping pong e
+        pallavolo (2) <i class="fa-solid fa-arrow-right"></i> Il torneo lo
+        vincono i Dragons
       </p>
     </MainLayout>
   );
