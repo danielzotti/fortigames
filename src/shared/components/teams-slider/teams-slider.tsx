63b65bcb8141fd7b684272f9f2ab342e3601b0ac
Stiv <stiv@stivnet.net>
2023-09-28 00:49:36 +0200

    teams component

---
+++ src/shared/components/teams-slider/teams-slider.tsx
@@ -0,0 +1,29 @@
+import {Slot, component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
+import styles from "./teams-slider.module.scss";
+
+export default component$(() => {
+
+  useVisibleTask$(async () => {
+
+  });
+
+  return (
+    <div class={styles.teamsSliderContainer}>
+      <div class={styles.teamsBgContainer}>
+        <div class={[styles.teamCover, styles.tigers]}></div>
+        <div class={[styles.teamCover, styles.dragons]}></div>
+      </div>
+      <div class={styles.slotContainer}>
+          <div class={[styles.teamInfoContainer, styles.tigers]}>
+              <div class={styles.teamTitle}>Tigers</div>
+          </div>
+          <div class={[styles.teamInfoContainer, styles.dragons]}>
+              <div class={styles.teamTitle}>Dragons</div>
+          </div>
+          <div class={styles.slot}>
+              <Slot></Slot>
+          </div>
+      </div>
+    </div>
+  );
+});
