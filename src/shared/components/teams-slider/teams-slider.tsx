7a83008dd7110881dfd7a94e11cf6ed0e65f830a
Daniel Zotti <daniel.zotti87@gmail.com>
2023-09-28 01:21:46 +0200

    Fix vari

--- src/shared/components/teams-slider/teams-slider.tsx
+++ src/shared/components/teams-slider/teams-slider.tsx
@@ -1,12 +1,7 @@
-import {Slot, component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
+import { Slot, component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import styles from "./teams-slider.module.scss";
 
 export default component$(() => {
-
-  useVisibleTask$(async () => {
-
-  });
-
   return (
     <div class={styles.teamsSliderContainer}>
       <div class={styles.teamsBgContainer}>
@@ -14,15 +9,15 @@ export default component$(() => {
         <div class={[styles.teamCover, styles.dragons]}></div>
       </div>
       <div class={styles.slotContainer}>
-          <div class={[styles.teamInfoContainer, styles.tigers]}>
-              <div class={styles.teamTitle}>Tigers</div>
-          </div>
-          <div class={[styles.teamInfoContainer, styles.dragons]}>
-              <div class={styles.teamTitle}>Dragons</div>
-          </div>
-          <div class={styles.slot}>
-              <Slot></Slot>
-          </div>
+        <div class={[styles.teamInfoContainer, styles.tigers]}>
+          <div class={styles.teamTitle}>Tigers</div>
+        </div>
+        <div class={[styles.teamInfoContainer, styles.dragons]}>
+          <div class={styles.teamTitle}>Dragons</div>
+        </div>
+        <div class={styles.slot}>
+          <Slot></Slot>
+        </div>
       </div>
     </div>
   );
