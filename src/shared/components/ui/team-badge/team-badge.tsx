ea3ff4ee365c26480b4bbb03cbeb250346e3d725
erikfazio <fazioerik@gmail.com>
2023-09-27 14:23:26 +0200

    Implement Team Badge

---
+++ src/shared/components/ui/team-badge/team-badge.tsx
@@ -0,0 +1,23 @@
+import { component$ } from "@builder.io/qwik";
+import styles from "./team-badge.module.scss";
+import { useNavigate } from "@builder.io/qwik-city";
+import { useAuth } from "~/hooks/useAuth";
+
+interface Props {
+  team?: "dragons" | "tigers";
+}
+
+export default component$(({ team }: Props) => {
+  const { auth } = useAuth();
+  const navigate = useNavigate();
+
+  return (
+    <div
+      class={`${styles.teamBadge} ${
+        team === "dragons" ? styles.dragons : styles.tigers
+      }`}
+    >
+      {team === "dragons" ? "竜" : "虎"}
+    </div>
+  );
+});
--- src/shared/layouts/main-layout/main-layout.tsx
+++ src/shared/layouts/main-layout/main-layout.tsx
@@ -1,8 +1,10 @@
-import { component$, Slot } from "@builder.io/qwik";
+import { component$, Slot, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import ProfileAvatar from "~/shared/components/ui/profile-avatar/profile-avatar";
 import BottomNavigation from "~/shared/components/ui/bottom-navigation/bottom-navigation";
 
 import styles from "./main-layout.module.scss";
+import { useAuth } from "~/hooks/useAuth";
+import { supabaseClient } from "~/supabase/supabase-client";
 
 interface Props {
   title?: string;
@@ -10,12 +12,28 @@ interface Props {
 }
 
 export default component$(({ title, hasContentPaddingTop = true }: Props) => {
+  const { auth } = useAuth();
+  const team = useSignal<"tigers" | "dragons" | null>(null);
+
+  useVisibleTask$(async () => {
+    if (auth.value?.user.email) {
+      const { data } = await supabaseClient
+        .from("users")
+        .select("team")
+        .eq("email", auth.value.user.email);
+
+      if (data) {
+        team.value = data[0].team;
+      }
+    }
+  });
+
   return (
     <div class={styles.container}>
       <div class={styles.top}>
         <div></div>
         {title && <h1 class={styles.title}>{title}</h1>}
-        <ProfileAvatar />
+        <ProfileAvatar team={team.value} />
       </div>
       <div
         class={`${styles.content} ${
