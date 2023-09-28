e1c709d6d5be4d0b17337e940f6ade8f26039f14
erikfazio <fazioerik@gmail.com>
2023-09-28 15:23:47 +0200

    Implement login background

--- src/routes/login/layout.tsx
+++ src/routes/login/layout.tsx
@@ -1,10 +1,11 @@
 import { component$, Slot } from "@builder.io/qwik";
-
 import styles from "./layout.module.scss";
 
 export default component$(() => {
   return (
     <div class={styles.container}>
+      <div class={styles.coverTiger}></div>
+      <div class={styles.coverDragon}></div>
       <div class={styles.content}>
         <Slot />
       </div>
