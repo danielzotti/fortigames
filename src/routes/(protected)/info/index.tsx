1140c7d28a9ee2993c677dc5619634cfffc766d1
Daniel Zotti <daniel.zotti87@gmail.com>
2023-09-28 17:37:48 +0200

    Fix info page

--- src/routes/(protected)/info/index.tsx
+++ src/routes/(protected)/info/index.tsx
@@ -53,13 +53,11 @@ export default component$(() => {
             <div class="agenda-item-text"> {e.activity}</div>
           </div>
         ))}
-
         <h3>Sabato 30</h3>
         <div class="agenda-item">
           <div class="agenda-item-time">MAX 11:00</div>
           <div class="agenda-item-text">Check-out</div>
         </div>
-
         <h2 id="location">Location</h2>
         <p>
           <strong>West Garda Hotel</strong>
@@ -94,71 +92,119 @@ export default component$(() => {
           Vicino all’hotel ci sono gli aeroporti G. D’Annunzio di Brescia
           Montichiari (25 km) e Valerio Catullo di Verona Villafranca (40 Km)
         </p>
-
         <h2 id="torneo">Il torneo</h2>
         <p>
-        Due squadre Tigers e Dragon si affronteranno in tre competizioni: <br/>
-        Calcio a 5<br/>
-        Volley<br/>
-        Ping Pong<br/>
-        Sarà decretata vincitrice del torneo la squadra che avrà vinto almeno due competizioni.<br/>
+          Due squadre Tigers e Dragon si affronteranno in tre competizioni:{" "}
+          <br />
+          Calcio a 5<br />
+          Volley
+          <br />
+          Ping Pong
+          <br />
+          Sarà decretata vincitrice del torneo la squadra che avrà vinto almeno
+          due competizioni.
+          <br />
         </p>
         <p>
-        La squadra vincitrice riceverà in premio la preziosa 'Taoscimmia Maya', il capolavoro del maestro Gianluca La Rosa in copia unica.<br/>
-        Il torneo inizierà alle 17:30, o comunque dopo le presentazioni e si concluderà tassativamente alle 19:20 per consentire alle eventuali sfide in parità di concludersi secondo le modalità sottoindicate e di prepararsi per la cena.<br/>
-
+          La squadra vincitrice riceverà in premio la preziosa 'Taoscimmia
+          Maya', il capolavoro del maestro Gianluca La Rosa in copia unica.
+          <br />
+          Il torneo inizierà alle 17:30, o comunque dopo le presentazioni e si
+          concluderà tassativamente alle 19:20 per consentire alle eventuali
+          sfide in parità di concludersi secondo le modalità sottoindicate e di
+          prepararsi per la cena.
+          <br />
         </p>
         <p>
-          <Button isLink={true} href="/public/static/documents/RegolamentoTorneo.pdf" target="_blank">
-          Scarica il regolamento completo
+          <Button
+            isLink={true}
+            href="/public/static/documents/RegolamentoTorneo.pdf"
+            target="_blank"
+          >
+            Scarica il regolamento completo
           </Button>
-         
         </p>
         <p>
-          <Button isLink={true} href="/public/static/documents/RegolamentoTorneo.pdf" target="_blank">
-          scarica il regolamento completo
+          <Button
+            isLink={true}
+            href="/public/static/documents/RegolamentoTorneo.pdf"
+            target="_blank"
+          >
+            scarica il regolamento completo
           </Button>
-         
         </p>
-
-
         <h4>Calcio</h4>
         <p>
-        Si sfideranno due squadre composte da 9 giocatori, 5 in campo e 4 riserve.<br/>
-        Le squadre sono libere di decidere se tenere il portiere fisso o se effettuare una rotazione per questo ruolo.<br/>
-        Ogni 10 minuti ci saranno 2 sostituzioni.<br/>
-        Vincerà la squadra che alle 19:20 avrà segnato più gol; in caso di parità si procederà con i calci di rigore (5 ed eventualmente ad oltranza).
-                </p>
-                <h4>Volley</h4>
-                <p>
-                Si sfideranno due squadre composte da 13 giocatori, 6 in campo e 7 riserve.<br/>
-        Le squadre sono libere di ruotare nelle varie posizioni o di mantenere posizioni fisse.<br/>
-        Ogni 10 minuti ci saranno 4 sostituzioni.<br/>
-        Vincerà la squadra che alle 19:20 avrà vinto più set (al meglio dei 25 punti); in caso di parità si procederà al tie break (al meglio dei 15 punti)
-                </p>
-                <h4>Ping pong</h4>
-                <p>
-                Si sfideranno due squadre composte da 14 giocatori.<br/>
-        Ci saranno 14 sfide al meglio dei 21 punti.<br/>
-        Si potranno giocare 2 sfide contemporaneamente vista la presenza di due tavoli.<br/>
-        Vincerà la squadra che alle 19:20 avrà vinto più sfide; in caso di parità si procederà con una sfida secca, ogni squadra sceglierà chi schierare per lo spareggio.
-                </p>
-
-
-                Facilitatori
-                <p>Ogni competizione avrà un facilitatore, questi avrà il compito di:</p>
-                <ul>
-                  <li>verificare il corretto svolgimento dei giochi</li>
-                <li>segnalare eventuali comportamenti scorretti (falli, provocazioni gratuite, insulti, ecc) che saranno sanzionati con il cartellino giallo (ammonizione) o rosso (espulsione) secondo il suo insindacabile giudizio; un doppio cartellino giallo porterà al cartellino rosso e all’espulsione</li>
-              <li>conteggiare i gol/punti</li>
-              </ul>
-        <p>Il facilitatore della partita di calcio a 5 dovrà inoltre:
-        <li>chiamare il cambio ogni 10 minuti tramite avvisatore acustico (fischietto o voce) e verificare che le sostituzioni siano effettuate</li>
-        <li>appuntare il nome dei marcatori</li>
+          Si sfideranno due squadre composte da 9 giocatori, 5 in campo e 4
+          riserve.
+          <br />
+          Le squadre sono libere di decidere se tenere il portiere fisso o se
+          effettuare una rotazione per questo ruolo.
+          <br />
+          Ogni 10 minuti ci saranno 2 sostituzioni.
+          <br />
+          Vincerà la squadra che alle 19:20 avrà segnato più gol; in caso di
+          parità si procederà con i calci di rigore (5 ed eventualmente ad
+          oltranza).
+        </p>
+        <h4>Volley</h4>
+        <p>
+          Si sfideranno due squadre composte da 13 giocatori, 6 in campo e 7
+          riserve.
+          <br />
+          Le squadre sono libere di ruotare nelle varie posizioni o di mantenere
+          posizioni fisse.
+          <br />
+          Ogni 10 minuti ci saranno 4 sostituzioni.
+          <br />
+          Vincerà la squadra che alle 19:20 avrà vinto più set (al meglio dei 25
+          punti); in caso di parità si procederà al tie break (al meglio dei 15
+          punti)
+        </p>
+        <h4>Ping pong</h4>
+        <p>
+          Si sfideranno due squadre composte da 14 giocatori.
+          <br />
+          Ci saranno 14 sfide al meglio dei 21 punti.
+          <br />
+          Si potranno giocare 2 sfide contemporaneamente vista la presenza di
+          due tavoli.
+          <br />
+          Vincerà la squadra che alle 19:20 avrà vinto più sfide; in caso di
+          parità si procederà con una sfida secca, ogni squadra sceglierà chi
+          schierare per lo spareggio.
+        </p>
+        Facilitatori
+        <p>
+          Ogni competizione avrà un facilitatore, questi avrà il compito di:
+        </p>
+        <ul>
+          <li>verificare il corretto svolgimento dei giochi</li>
+          <li>
+            segnalare eventuali comportamenti scorretti (falli, provocazioni
+            gratuite, insulti, ecc) che saranno sanzionati con il cartellino
+            giallo (ammonizione) o rosso (espulsione) secondo il suo
+            insindacabile giudizio; un doppio cartellino giallo porterà al
+            cartellino rosso e all’espulsione
+          </li>
+          <li>conteggiare i gol/punti</li>
+        </ul>
+        <p>Il facilitatore della partita di calcio a 5 dovrà inoltre:</p>
+        <ul>
+          <li>
+            chiamare il cambio ogni 10 minuti tramite avvisatore acustico
+            (fischietto o voce) e verificare che le sostituzioni siano
+            effettuate
+          </li>
+          <li>appuntare il nome dei marcatori</li>
         </ul>
         <p>Il facilitatore della partita di volley dovrà inoltre:</p>
         <ul>
-          <li>chiamare il cambio ogni 10 minuti tramite avvisatore acustico (fischietto o voce) e verificare che le sostituzioni siano effettuate</li>
+          <li>
+            chiamare il cambio ogni 10 minuti tramite avvisatore acustico
+            (fischietto o voce) e verificare che le sostituzioni siano
+            effettuate
+          </li>
         </ul>
       </div>
       <BackTopButton ref={containerRef} />
